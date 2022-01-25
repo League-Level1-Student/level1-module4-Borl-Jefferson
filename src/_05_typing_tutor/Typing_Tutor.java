b0295a01c8883d8b0449e7fa9495374410e29818
Blast <Blast@Gameinator.lan>
2022-01-24 18:29:20 -0800

    x

--- src/_05_typing_tutor/Typing_Tutor.java
+++ src/_05_typing_tutor/Typing_Tutor.java
@@ -1,27 +1,76 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Typing_Tutor {
+public class Typing_Tutor implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JLabel jell = new JLabel;
+	JLabel jell = new JLabel();
+	String string = ".";
+	int score;
+	int perc;
+	int oscore;
+	int fscore;
+
 	char generateRandomLetter() {
 	    Random r = new Random();
-	    return (char) (r.nextInt(26) + 'a');
+	    return (char) (r.nextInt(26) + 'A');
 	}
 	char currentLetter;
-public static void main(String[] args) {
-	
-}
+
+
 void setup(){
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	currentLetter = generateRandomLetter();
-	jell.setText(currentLetter);
+	
+	jell.setText(currentLetter + string);
+	jell.setFont(jell.getFont().deriveFont(400.0f));
+	jell.setHorizontalAlignment(JLabel.CENTER);
+	frame.addKeyListener(this);
+	panel.add(jell);
+	frame.add(panel);
 }
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
 }
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	score++;
+	System.out.println(fscore);
+	if ((Character.toLowerCase(e.getKeyChar())) ==
+			(Character.toLowerCase(currentLetter))) {
+	currentLetter = generateRandomLetter();
+	jell.setText(currentLetter + string);
+	panel.setBackground(Color.GREEN);
+	}
+	else {
+		panel.setBackground(Color.RED);	
+		
+		
+	oscore=score-1;
+	perc=100/score;
+		
+	fscore=perc*oscore;
+	}
+	}
+
+
+}
+
+
