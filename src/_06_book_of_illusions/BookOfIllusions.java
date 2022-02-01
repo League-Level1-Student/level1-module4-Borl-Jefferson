64e21c5f0cfd88da3613defead1b59ad51765e5f
Blast <Blast@Gameinator.lan>
2022-01-31 18:28:11 -0800

    fghty

--- src/_06_book_of_illusions/BookOfIllusions.java
+++ src/_06_book_of_illusions/BookOfIllusions.java
@@ -11,6 +11,7 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
@@ -18,24 +19,43 @@ import javax.swing.JLabel;
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+	 JFrame jeff = new JFrame();
+	JLabel jell;
+String il2 = "ilusion2.jpg";
 	public void run() {
 		// 2. make the frame visible
-		// 3. set the size of the frame
+		jeff.setVisible(true);
+		// 3. set the size of -the frame
+		jeff.setSize(1920, 1080);
 		// 4. find 2 images and save them to your project’s _06_book_of_illusions folder
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String il1 = "ilusion1.jpg";
+		
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		jell = loadImageFromComputer(il1);
 		// 8. add your JLabel to the frame
+		jeff.add(jell);
 		// 9. call the pack() method on the frame
+		jeff.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		jeff.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked");
 		// 12. remove everything from the frame that was added earlier
+		jeff.getContentPane().removeAll();
 		// 13. load a new image like before (this is more than one line of code)
-		// 14. pack the frame
+		jell = loadImageFromComputer(il2);
+		jeff.add(jell);
+		jeff.pack();
+		
+		
+		
+		
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
--- src/_07_fortune_teller/FortuneTellerRunner.java
+++ src/_07_fortune_teller/FortuneTellerRunner.java
@@ -1,4 +1,5 @@
 package _07_fortune_teller;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 /*
@@ -7,13 +8,24 @@ import javax.swing.SwingUtilities;
  */
 
 public class FortuneTellerRunner {
-    public static void main(String[] args) throws Exception {
-      	 SwingUtilities.invokeLater(new FortuneTeller());
-      	 begin();
-       }
+
+       
     
     static void begin() {
       	 // Welcome the user. Give them a hint for the secret location.
-
+JOptionPane.showMessageDialog(null, "The game is simple.");
+JOptionPane.showMessageDialog(null, "One goal, find Waldo.");
+JOptionPane.showMessageDialog(null, "He has been evading his taxes for years.");
+JOptionPane.showMessageDialog(null, "We have sent out many images were people have claimed to see him.");
+JOptionPane.showMessageDialog(null, "We have put our best people on it, it wasnt working...");
+JOptionPane.showMessageDialog(null, "So we published the images for everyone to find him, and gues what?");
+JOptionPane.showMessageDialog(null, "People have made fake images since the internet made it into a game.");
+JOptionPane.showMessageDialog(null, "There are so many fake sightings online but we recently got a sighting from someone working here.");
+JOptionPane.showMessageDialog(null, "This is the most recent Waldo sighting, good luck finding him. You are going to need it.");
+JOptionPane.showMessageDialog(null, "Hint* click anywere on the image to get a hint!");
     }
+    public static void main(String[] args) throws Exception {
+     	 SwingUtilities.invokeLater(new FortuneTeller());
+     	 begin();
+      }
 }
