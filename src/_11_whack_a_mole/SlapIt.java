6d5a3f6a9e748dcf11c6f25659727124f4a1347a
league <league@imac-34.attlocal.net>
2022-03-07 18:30:23 -0800

    df fb

--- src/_11_whack_a_mole/SlapIt.java
+++ src/_11_whack_a_mole/SlapIt.java
@@ -1,5 +1,28 @@
 package _11_whack_a_mole;
 
-public class SlapIt {
+import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SlapIt {
+	String set = "";
+JFrame jeff = new JFrame();
+JPanel jepp = new JPanel();
+
+
+
+public static void main(String[] args) {
+	Random r = new Random();
+int rf= r.nextInt(11);
+for (int i = 0; i < 12; i++) {
+	if(i == rf) {
+		
+	}
+}
+
+
+
+}
 }
